94f59a9ea869bf1b70bcdacf8798737f55f58bdb
Bumsoo Kim <kbeom00@gmail.com>
2024-05-26 03:50:23 +0900

    add text_to_html function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,4 +1,5 @@
 import unittest
+from textnode import text_node_to_html_node
 
 from textnode import TextNode
 
@@ -9,7 +10,12 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
-
+    def test_text_to_html(self):
+        text1 = TextNode("This is bold", "bold")
+        html1 = text_node_to_html_node(text1)
+        print(text1, html1)
+        self.assertEqual(html1, "<b>This is bold</b>")
+        
 if __name__ == "__main__":
     unittest.main()
 
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,13 @@
+from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold= "bold"
+text_type_italic= "italic"
+text_type_code= "code"
+text_type_link= "link"
+text_type_image= "image"
+
+# markdown text
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -9,3 +19,23 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+# function that convert text to html
+def text_node_to_html_node(text_node):
+    text = text_node.text
+    text_type = text_node.text_type
+    url = text_node.url
+
+    if text_type == text_type_text:
+        return LeafNode(None, text).to_html()
+    if text_type == text_type_bold:
+        return LeafNode("b", text).to_html()
+    if text_type == text_type_italic:
+        return LeafNode("i", text).to_html()
+    if text_type == text_type_code:
+        return LeafNode("code", text).to_html()
+    if text_type == text_type_link:
+        return LeafNode("a", text, {"href": url}).to_html()
+    if text_type == text_type_image:
+        return LeafNode("img", "", {"src": url, "alt": text}).to_html()
+    raise Exception("Invalid text type")
