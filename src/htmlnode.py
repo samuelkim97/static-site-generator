a0ae5504f2b4c4700cbaab157a72126a0f9b070b
Bumsoo Kim <kbeom00@gmail.com>
2024-05-26 01:26:30 +0900

    add leafnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,20 +1,34 @@
+# basic structure
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=None, props=None):
+    def __init__(self, tag=None, value=None, props=None, children=None):
         self.tag = tag # tag name
         self.value = value # content of tag
-        self.children = children # list
         self.props = props # dictionary
+        self.children = children # list
 
     def to_html(self):
         raise NotImplementedError()
 
     def props_to_html(self):
-        li = []
-        for key, value in self.props.items():
-            li.append("".join([key, value]))
-        return " ".join(li) 
+        if self.props == None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f" {prop}=\"{self.props[prop]}\""
+        return props_html
 
     def __repr__(self):
         str = f"tag: {self.tag} value: {self.value} children: {self.children} props: {self.props}"
         return str
 
+# html with no children
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf node requires a value.")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,11 +1,20 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_node(self):
         html = HTMLNode("h1", "a")
         self.assertTrue(HTMLNode())
+
+    def test_leaf(self):
+        leaf1 = LeafNode("p", "This is a paragraph of text.").to_html()
+        leaf2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
+        print(leaf1)
+        print(leaf2)
+        self.assertTrue(leaf1)
+        self.assertTrue(leaf2)
+
 
 
 if __name__ == "__main__":
