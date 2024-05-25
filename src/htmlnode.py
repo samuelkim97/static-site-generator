f4292b626a0296cf96eb34f1217b8842b6355d6e
Bumsoo Kim <kbeom00@gmail.com>
2024-05-26 02:30:54 +0900

    add parentnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,10 +1,10 @@
 # basic structure
 class HTMLNode:
-    def __init__(self, tag=None, value=None, props=None, children=None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag # tag name
         self.value = value # content of tag
-        self.props = props # dictionary
         self.children = children # list
+        self.props = props # dictionary
 
     def to_html(self):
         raise NotImplementedError()
@@ -23,8 +23,8 @@ class HTMLNode:
 
 # html with no children
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
-        super().__init__(tag, value, props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value == None:
@@ -32,3 +32,20 @@ class LeafNode(HTMLNode):
         if self.tag == None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+# TODO: PARENT NODE
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("There is no tag.")
+        if self.children == None:
+            raise ValueError("There is no children")
+        # TODO: return string of html with children - use recursion
+        # <p><b>bold text</b>Normal text<i>italic</i>Normal text</p>
+        child_html = ""
+        for leafnode in self.children:
+            child_html += leafnode.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{child_html}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_node(self):
@@ -15,6 +15,16 @@ class TestHTMLNode(unittest.TestCase):
         self.assertTrue(leaf1)
         self.assertTrue(leaf2)
 
+    def test_parent(self):
+        parent = ParentNode("p", [LeafNode("b", "Bold text"), LeafNode(None, "Normal text"), LeafNode("i", "italic text"), LeafNode(None, "Normal text"),]).to_html()
+        print(parent)
+        parent2 = ParentNode("html", [
+            ParentNode("h1", [LeafNode("p", "This is h1"), LeafNode("a", "image", {"href": "www.google.com", "alt": "alt"})]),
+            LeafNode("p", "This is paragraph")
+        ]).to_html()
+        print(parent2)
+        self.assertEqual(parent, "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+        self.assertEqual(parent2, "<html><h1><p>This is h1</p><a href=\"www.google.com\" alt=\"alt\">image</a></h1><p>This is paragraph</p></html>")
 
 
 if __name__ == "__main__":
