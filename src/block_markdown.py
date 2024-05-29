2c55792cb54923d967de1b4f2710a4b60367fdf4
Bumsoo Kim <kbeom00@gmail.com>
2024-05-29 23:11:33 +0900

    done extract_title func.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -39,7 +39,7 @@ def block_to_block_type(single_block):
     if all(ol_regex.search(line) for line in splitted):
         return block_type_ol
     return block_type_paragraph
-# NOTE: DONE!
+
 def block_to_html_node(single_block):
     block_type = block_to_block_type(single_block)
     # HEADING
@@ -63,7 +63,6 @@ def block_to_html_node(single_block):
 
 
 
-# NOTE: DONE!
 # HEADING
 def heading_to_html_node(block):
     return_list = []
@@ -79,14 +78,14 @@ def code_to_html_node(block):
     replaced_block = block.replace("`", "")
     replaced_string = " ".join(replaced_block.split("\n"))
     child = text_to_textnodes(replaced_string)
-    return ParentNode("pre", [ParentNode("code", child)])
+    return [ParentNode("pre", [ParentNode("code", child)])]
 
 # QUOTE
 def quote_to_html_node(block):
     pure_text = block.replace(">", "")
     string = " ".join(pure_text.split("\n"))
     child = text_to_textnodes(string)
-    return ParentNode("blockquote", child)
+    return [ParentNode("blockquote", child)]
 
 # UL
 def ul_to_html_node(block):
@@ -100,7 +99,7 @@ def ul_to_html_node(block):
     child = []
     for line in after:
         child.append(ParentNode("li", text_to_textnodes(line)))
-    return ParentNode("ul", child)
+    return [ParentNode("ul", child)]
 # OL
 def ol_to_html_node(block):
     after = []
@@ -108,15 +107,15 @@ def ol_to_html_node(block):
         after.append(line.replace(r"^\d\.\s", ""))
     after_string= " ".join(after)
     child = text_to_textnodes(after_string)
-    return ParentNode("ol", [ParentNode("li", child)])
+    return [ParentNode("ol", [ParentNode("li", child)])]
 
 # PARAGRAPH
 def paragraph_to_html_node(block):
     paragraph = " ".join(block.split("\n"))
     child = text_to_textnodes(paragraph)
-    return ParentNode("p", child)
+    return [ParentNode("p", child)]
 
-# TODO: markdown to html !!!
+
 def markdown_to_html_node(markdown):
     list_of_blocks = []
     blocks = markdown_to_blocks(markdown)
@@ -125,25 +124,40 @@ def markdown_to_html_node(markdown):
     return ParentNode("div", list_of_blocks)
 
 
+def extract_title(markdown):
+    for block in markdown_to_html_node(markdown).children:
+        for line_node in block:
+            if line_node.tag == "h1":
+                heading = ""
+                list_of_h1 = line_node.children
+                for text_node in list_of_h1:
+                    heading += text_node.text
+                heading = heading.lstrip(" ")
+                return heading
 
 
-    return
+    raise Exception("All pages need a single h1 header.")
+        #for parent_node in block:
+            #print(parent_node)
+    #raise Exception("All pages need a h1 header.")
+
 # TEST
-markdown1 = """# This is heading
+#markdown1 = """# This is heading
 ## and 2
 
-* this is unordered list.
-- this too
+#* this is unordered list.
+#- this too
 
-`print("This is code")
-x = 2 + 2
-print(x)`
+#`print("This is code")
+#x = 2 + 2
+#print(x)`
 
-Last, this is with **bold text** and *italic*
-and of course ![image](~/home/picture/pic.jpg)
-[link](https://www.google.com) link too!
-"""
-print(markdown_to_html_node(markdown1))
+#Last, this is with **bold text** and *italic*
+#and of course ![image](~/home/picture/pic.jpg)
+#[link](https://www.google.com) link too!
+#"""
+#print(extract_title(markdown1))
+#print(markdown_to_html_node(markdown1).children)
 #print(block_to_html_node("# This is heading.\n###### and also new line."))
 #print(block_to_html_node("```This is code.\n with many\nline\nhahaha.```"))
 # split_nodes_delimiter INVALID SYNTAX ERROR INLINE..py
--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
+import os, shutil
 from textnode import TextNode
 
 def main():
-    dummie = TextNode("This is a text node","bold","https://www.boot.dev")
-    print(dummie)
+    pass
 
-
+def copy_dir_to_dir(directory, dst):
+    # if given dir exists.
+    if os.path.exists(directory):
+        # loop through child
+        for child in os.listdir(directory):
+            # make child path from child name
+            child_path = os.path.join(directory, child)
+            if os.path.isfile(child_path):
+                shutil.copy(child_path, dst)
+                print(f"Successfully Copied: \"{child_path}\" to \"{dst}\"")
+            if os.path.isdir(child_path):
+                new_dst = os.path.join(dst, child)
+                print(f"=> Moved to \"{new_dst}\"")
+                os.mkdir(new_dst)
+                copy_dir_to_dir(child_path, new_dst)
+            
 main()
+
+# TEST
+copy_dir_to_dir("/home/bumsookim/Test/", "/home/bumsookim/workspace/github.com/samuelkim97/static-site-generator/public/")
