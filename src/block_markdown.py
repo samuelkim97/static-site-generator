f2c9b7120b8de8c76afb8112eb732d1a6249b65a
Bumsoo Kim <kbeom00@gmail.com>
2024-05-29 20:25:08 +0900

    add markdown_to_html func.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,6 @@
 import re
+from htmlnode import HTMLNode, ParentNode
+from inline_markdown import text_to_textnodes
 
 block_type_paragraph = "paragraph"
 block_type_heading= "heading"
@@ -37,11 +39,89 @@ def block_to_block_type(single_block):
     if all(ol_regex.search(line) for line in splitted):
         return block_type_ol
     return block_type_paragraph
+# NOTE: DONE!
+def block_to_html_node(single_block):
+    block_type = block_to_block_type(single_block)
+    # HEADING
+    if block_type == block_type_heading:
+        return heading_to_html_node(single_block)
+    # CODE
+    if block_type == block_type_code:
+        return code_to_html_node(single_block)
+    # QUOTE
+    if block_type == block_type_quote:
+        return quote_to_html_node(single_block)
+    # UL
+    if block_type == block_type_ul:
+        return ul_to_html_node(single_block)
+    # OL
+    if block_type == block_type_ol:
+        return ol_to_html_node(single_block)
+    # PARAGRAPH
+    if block_type == block_type_paragraph:
+        return paragraph_to_html_node(single_block)
+
+
+
+# NOTE: DONE!
+# HEADING
+def heading_to_html_node(block):
+    return_list = []
+    for line in block.split("\n"):
+        tag = f"h{line.count("#")}"
+        value = line.lstrip("#")
+        list_of_child = text_to_textnodes(value)
+        return_list.append(ParentNode(tag, list_of_child))
+    return return_list
+
+# CODE
+def code_to_html_node(block):
+    replaced_block = block.replace("`", "")
+    replaced_string = " ".join(replaced_block.split("\n"))
+    child = text_to_textnodes(replaced_string)
+    return ParentNode("pre", [ParentNode("code", child)])
+
+# QUOTE
+def quote_to_html_node(block):
+    pure_text = block.replace(">", "")
+    string = " ".join(pure_text.split("\n"))
+    child = text_to_textnodes(string)
+    return ParentNode("blockquote", child)
+
+# UL
+def ul_to_html_node(block):
+    # split line by line and delete * , - 
+    after = []
+    for line in block.split("\n"):
+        after.append(line.lstrip("* ").lstrip("- "))
+    # delelted *, - lines => after
+
+    # change line to HTMLNode
+    child = []
+    for line in after:
+        child.append(ParentNode("li", text_to_textnodes(line)))
+    return ParentNode("ul", child)
+# OL
+def ol_to_html_node(block):
+    after = []
+    for line in block.split("\n"):
+        after.append(line.replace(r"^\d\.\s", ""))
+    after_string= " ".join(after)
+    child = text_to_textnodes(after_string)
+    return ParentNode("ol", [ParentNode("li", child)])
+
+# PARAGRAPH
+def paragraph_to_html_node(block):
+    paragraph = " ".join(block.split("\n"))
+    child = text_to_textnodes(paragraph)
+    return ParentNode("p", child)
 
 # TEST
-#print(block_to_block_type("# This is heading.\n###### and also new line."))
-#print(block_to_block_type("```This is code.\n with many\nline\nhahaha.```"))
-#print(block_to_block_type(">This is code.\n> with many\n>line\n>hahaha.```"))
-#print(block_to_block_type("* This is code.\n* with many\n- line\n- hahaha.```"))
-#print(block_to_block_type("1. This is code.\n2. with many\n3. line\n4. hahaha.```"))
-#print(block_to_block_type("This is code.\nwith many\nline\n4. hahaha.```"))
+
+#print(block_to_html_node("# This is heading.\n###### and also new line."))
+#print(block_to_html_node("```This is code.\n with many\nline\nhahaha.```"))
+# TODO: 
+# split_nodes_delimiter INVALID SYNTAX ERROR INLINE..py
+print(block_to_html_node("* This is first item with `code block` inside\n* second item!\n- third item\n- last item.```"))
+#print(block_to_html_node("1. This is code.\n2. with many\n3. line\n4. hahaha.```"))
+#print(block_to_html_node("This is code.\nwith many\nline\n4. hahaha.```"))
--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -40,17 +40,20 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result = []
     for old_node in old_nodes:
         after_split_list = old_node.text.split(delimiter)
-        if len(after_split_list) % 2 == 0:
-            raise ValueError("Invalid Syntax Error.")
-
-        if len(after_split_list) != 1:
+        num_of_delimiter = old_node.text.count(delimiter)
+        if old_node.text == "":
+            continue
+        
+        # CASE1: if oldnode doesn't have delimiter or invalid syntax
+        elif num_of_delimiter == 0 or (num_of_delimiter % 2 == 1):
+            result.append(old_node)
+        # CASE2: if oldnode have right syntax
+        elif old_node.text.count(delimiter) % 2 == 0:
             for part in after_split_list:
                 if part in find_match_text(old_node.text, delimiter):
                     result.append(TextNode(part, text_type))
                 else:
                     result.append(TextNode(part, text_type_text))
-        if len(after_split_list) == 1:
-            result.append(old_node)
 
     return result
 
@@ -109,15 +112,14 @@ def text_to_textnodes(text):
     c = split_nodes_delimiter(b, "`", text_type_code)
     d = split_nodes_delimiter(c, "**", text_type_bold)
     e = split_nodes_delimiter(d, "*", text_type_italic)
-    pprint.pp(e)
     return e
     
 
-#text_to_textnodes("This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)")
+final = text_to_textnodes("This is **text** with\n an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)")
 
-
+#print(final)
 #node = TextNode(
-#    "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+#    "This is\n text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
 #    text_type_text,
 #)
 #new_nodes = split_nodes_image([node])
